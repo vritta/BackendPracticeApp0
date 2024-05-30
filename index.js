289cf49e9beb057a1c528b85fabaf8f21733e382
vritta <anuhartripathi15@gmail.com>
2024-05-30 22:33:41 +0530

    nodemon and post request

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//adding middleware
+app.use(express.json());
+
+//get request
+app.get("/", (req, res)=>{
+    res.send(`<h1>This is Heading</h1>`);
+});
+app.post("/car", (req, res)=>{
+    res.send("Received a post request");
+});
+
 app.listen(PORT, (req, res)=>{
     console.log(`App listening to port - ${PORT}`);
 });
